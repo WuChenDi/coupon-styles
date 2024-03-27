7ed6b71593cd6e043360e8cb8e151ee7de3d3521
wudi <wuchendi96@gmail.com>
2024-03-27 10:19:20 +0800

    feat: Modify components to become tsx files

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import { defineComponent } from 'vue'
+
+import Header from '~/components/header.vue'
+import Main from '~/components/main.vue'
+import Footer from '~/components/footer'
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    return () => (
+      <div>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    )
+  },
+})
